4af9ca50111219a9d3a8b8858e5932072f240d0a
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-02-14 13:17:03 -0700

    Added ControlFlow Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x= 50;
+        int y = 50;
+        if (x == y) {
+            System.out.println("This is equal");
+        }
+        else if (x > y){
+            System.out.println("x");
+        }
+        else {
+            System.out.println("y");
+        }
+    }
+}
